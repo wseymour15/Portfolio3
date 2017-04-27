095de962c4c335bacf192e3caeac9965e174499d
wseymour15 <walterseymour15@gmail.com>
2017-04-26 23:25:08 -0500

    more server stuff

--- socketServer.js
+++ socketServer.js
@@ -25,8 +25,16 @@
 //---------------------------------------------------------------
 var io = require('socket.io').listen(5000);
 var i,j;
-var board = new Array(8);
-var members = new Array(2);
+var b = new Array(8);
+var t = 0; 
+
+var mems = new Array(2);
+var mv = new Array(2);
+var bC = 12;
+var rC = 12;
+
+var curMem;
+
 var memCount = 0;
 //array of arrays (2D array)
 for(i = 0; i<8; i++){
@@ -36,14 +44,20 @@ for(i = 0; i<8; i++){
 //need double for loop to fill each position with a checker piece object
 for(i=0;i<8;i++){
     for(j=0;j<8;j++){
-		var piece = {color:0, king:0};
-		if(i < 3 && j % 2 == 0){
+		var piece = {color:0, king:0, selected:0, posX: j, posY: i};
+		if(i < 3 && i != 1 && j % 2 == 0){
 			piece.color = 1;
 		}
-		else if(i > 4 && j % 2 != 0){
+		else if(i < 3 && i == 1 && j % 2 != 0){
+			piece.color = 1;
+		}
+		else if(i > 4 && i != 6 && j % 2 != 0){
 			piece.color = 2;
 		}
-        board[i][j] = piece;
+		else if(i > 4 && i == 6 && j % 2 == 0){
+			piece.color = 2;
+		}
+        b[i][j] = piece;
     }
 }
 
@@ -54,18 +68,39 @@ io.sockets.on('connection', function(socket) {
   socket.on('joinGame', function(content){
 	if(memCount < 2){
 		console.log("A player joined!");
-		members[memCount] = socket.id;
-		console.log(socket.id);
-		console.log(members[0], members[1]);
+		mems[memCount] = socket.id;
+		console.log(mems[0], mems[1]);
 		++memCount;
 	}		
   });
 	
   socket.on('myEvent', function(content) {
     console.log(content); 
-    //io.sockets.emit('server', "This is the server: got your message");
-    io.sockets.emit('server', board);
+    io.sockets.emit('server', b);
     //io.to(members[0]).emit('message', 'for your eyes only');
   });
 	
 });
+
+var game = {board: b, turn: 0, members: mems, curMember : mems[0], moves: mv, blackCount: bC, redCount = rC};
+
+//Used to move a piece
+function swapPiece(){
+	var temp1 = b[data[0].posY][data[0].posX];
+	game.board[moves[0].posY][data[0].posX] = b[data[1].posY][data[1].posX];
+	b[data[1].posY][data[1].posX] = temp1;
+	
+	data[0] = 0;
+	data[1] = 0;
+	
+}
+
+function endGame(){
+	if(rC == 0){
+		//black wins
+	}
+	else if(bC == 0){
+		//red wins
+	}
+}
+
