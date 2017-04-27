a4d95dde371da567b221499a39b734d5a0ad6dd6
lucas-kern <lkern@iastate.edu>
2017-04-27 01:44:32 -0500

    Got basic movements working for black

--- socketServer.js
+++ socketServer.js
@@ -32,13 +32,14 @@ var mems = new Array(2);
 var mv = new Array(2);
 var bC = 12;
 var rC = 12;
+var game;
 
 var curMem;
 
 var memCount = 0;
 //array of arrays (2D array)
-for(i = 0; i<8; i++){
-    board[i] = new Array(8);
+for(i = 0; i < 8; i++){
+    b[i] = new Array(8);
 }
 
 //need double for loop to fill each position with a checker piece object
@@ -76,31 +77,91 @@ io.sockets.on('connection', function(socket) {
 	
   socket.on('myEvent', function(content) {
     console.log(content); 
+      getFirstClick(content);
     io.sockets.emit('server', b);
     //io.to(members[0]).emit('message', 'for your eyes only');
   });
 	
 });
 
-var game = {board: b, turn: 0, members: mems, curMember : mems[0], moves: mv, blackCount: bC, redCount = rC};
-
+if(game == undefined){
+    game = {board: b, turn: 1, members: mems, curMember : mems[0], moves: mv, blackCount: bC, redCount: rC};
+}
 //Used to move a piece
 function swapPiece(){
-	var temp1 = b[data[0].posY][data[0].posX];
-	game.board[moves[0].posY][data[0].posX] = b[data[1].posY][data[1].posX];
-	b[data[1].posY][data[1].posX] = temp1;
-	
-	data[0] = 0;
-	data[1] = 0;
-	
+    game.board[game.moves[0].posY][game.moves[0].posX].selected = 0;
+    var tempY =game.board[game.moves[0].posY][game.moves[0].posX].posY;
+    var tempX = game.board[game.moves[0].posY][game.moves[0].posX].posX;
+    game.board[game.moves[0].posY][game.moves[0].posX].posY = game.moves[1].posY;
+   game.board[game.moves[0].posY][game.moves[0].posX].posX = game.moves[1].posX;
+    game.board[game.moves[1].posY][game.moves[1].posX].posX = tempX;
+    game.board[game.moves[1].posY][game.moves[1].posX].posY = tempY;
+    
+	var temp1 = game.board[game.moves[0].posY][game.moves[0].posX];
+	game.board[game.moves[0].posY][game.moves[0].posX] = game.board[game.moves[1].posY][game.moves[1].posX];
+	game.board[game.moves[1].posY][game.moves[1].posX] = temp1;
+	game.moves[0] = undefined;
+	game.moves[1] = undefined;
+	return;
 }
 
 function endGame(){
-	if(rC == 0){
+	if(game.redCount == 0){
 		//black wins
 	}
-	else if(bC == 0){
+	else if(game.blackCount == 0){
 		//red wins
 	}
 }
 
+    function getFirstClick(piece){
+        if(game.members[game.turn-1] == game.curMember && piece.color == game.turn ){
+            if(game.moves[0] != undefined && game.moves[0] != piece){
+                game.board[game.moves[0].posY][game.moves[0].posX].selected = 0;
+            }
+            game.moves[0] = piece;
+            game.board[game.moves[0].posY][game.moves[0].posX].selected = 1;
+        }
+        else if(game.moves[0] != undefined && game.moves[1] == undefined){
+            getSecondClick(piece);
+        }
+            return;
+    }
+    
+    function getSecondClick(piece){
+        if(game.turn == 1){
+            if(piece.color == 0 && game.moves[0].posY + 1 == piece.posY && (game.moves[0].posX - 1 == piece.posX || game.moves[0].posX + 1 == piece.posX)){
+                game.moves[1] = piece;
+                swapPiece();
+            }
+            else if(piece.color == 0 && game.moves[0].posY + 1 == piece.posY && (game.moves[0].posX - 1 == piece.posX || game.moves[0].posY + 1 == piece.posX)){
+                
+            }
+            else if(piece.color == 0 && game.moves[0].posY + 1 == piece.posY && (game.moves[0].posX - 1 == piece.posX || game.moves[0].posX + 1 == piece.posX) && checkJump(piece)){
+                game.moves[1] = piece;
+                swapPiece();
+            }
+//            else{
+//                getFirstClick(piece);
+//            }
+        }
+        
+        return;
+    }
+    
+    function checkJump(piece){
+        first = game.moves[0];
+        if(game.turn == 1){
+            if(game.board[first.posY+2][first.posX-2] == piece && game.board[first.posY+1][first.posX-1].color == 2){
+                game.board[first.posY+1][first.posX-1].color = 0;
+                redCount--;
+                return 1;
+            }
+            else if (game.board[first.posY+2][first.posX+2] == piece && game.board[first.posY+1][first.posX+1].color == 2){
+                game.board[first.posY+1][first.posX+1].color = 0;
+                redCount--;
+                return 1;
+            }
+        }
+        return 0;
+    }
